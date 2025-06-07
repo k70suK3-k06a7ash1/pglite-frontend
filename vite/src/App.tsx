60cfc3678f37cd8b0ba74437f3a61b1250b3b962
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-06-08 01:01:23 +0900

    chore

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,22 +1,68 @@
-import { useEffect } from "react";
-import { Button } from "./components/ui/button";
-import { handleCommand } from "./packages/db/commands";
-import { getQuery } from "./packages/db/queries";
+import { useState } from 'react';
+import type { ChangeEvent, FormEvent } from 'react';
+import { usePGlite, useLiveQuery } from '@electric-sql/pglite-react'
+
+import './App.css';
+
+interface Todo {
+  id: number;
+  todo: string;
+  date: string;
+}
 
 function App() {
-	useEffect(() => {
-		(async() => {
-			const {result} = await getQuery()
-			console.log({result})
-		})()
-	},[])
-	return (
-		<>
-			<h1>sample pglite mock up </h1>
+  const [newTodoText, setNewTodoText] = useState<string>('');
+  const db = usePGlite();
+  const todos = useLiveQuery<Todo>(
+    'SELECT id, todo FROM todos_table ORDER BY date DESC'
+  );
 
-			<Button onClick={async() => await handleCommand()}>Command</Button>
-		</>
-	);
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>){
+    setNewTodoText(event.target.value);
+  }
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    
+    if (newTodoText.trim() === '') return;
+
+    await db.query('INSERT INTO todos_table (todo) VALUES ($1);', [newTodoText]);
+
+    setNewTodoText('');
+  }
+
+  function renderTodoList() {
+    return (
+      <ul className='todo-list'>
+        {todos ? todos.rows.map((todo) => (
+          <li key={todo.id} className='todo-item'>
+            {todo.todo}
+          </li>
+        )) : <>Loading...</>}
+      </ul>
+    );
+  }
+
+  return (
+    <div className='app-container'>
+      <h1>React Todo List</h1>
+      
+      <form onSubmit={handleSubmit} className='todo-form'>
+        <input
+          type='text'
+          value={newTodoText}
+          onChange={handleInputChange}
+          placeholder='Add a new todo'
+          className='todo-input'
+        />
+        <button type='submit' className='add-button'>
+          Add
+        </button>
+      </form>
+      
+      {renderTodoList()}
+    </div>
+  );
 }
 
 export default App;
--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -1,25 +1,29 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import { PGliteProvider } from "@electric-sql/pglite-react"
+import { PGliteProvider } from '@electric-sql/pglite-react';
+import { PGlite } from '@electric-sql/pglite';
+import { live } from '@electric-sql/pglite/live';
 
-import { PGlite } from "@electric-sql/pglite"
-import { live } from "@electric-sql/pglite/live"
+import App from './App.tsx';
 
+const db = await PGlite.create({
+  extensions: { live },
+  dataDir:'idb://my-pgdata',
+});
 
- const pgliteClient = await PGlite.create({
-    dataDir: 'idb://my-database',
-  extensions: { live }
-})
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS todos_table (
+    id serial PRIMARY KEY,
+    date timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    todo text
+  );
+`);
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<PGliteProvider db={pgliteClient}>
-			<App />
-
-		</PGliteProvider>
-		
-	</StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <PGliteProvider db={db}>
+      <App />
+    </PGliteProvider>
+  </StrictMode>,
+)
